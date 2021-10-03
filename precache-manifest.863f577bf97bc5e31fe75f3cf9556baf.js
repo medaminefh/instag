0b4b3146aabeeb8ae17fe691583d12a122aa61ea
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-10-03 14:11:41 +0100

    Updates

---
+++ precache-manifest.863f577bf97bc5e31fe75f3cf9556baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d43bfcc622ca08ee6c30805a6a72d9",
+    "url": "/instag/index.html"
+  },
+  {
+    "revision": "e52b26f4b7165225fbd3",
+    "url": "/instag/static/css/main.6351aa98.chunk.css"
+  },
+  {
+    "revision": "5cb40251f96f96a3da74",
+    "url": "/instag/static/js/2.015b404d.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/instag/static/js/2.015b404d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52b26f4b7165225fbd3",
+    "url": "/instag/static/js/main.b9f8b0e3.chunk.js"
+  },
+  {
+    "revision": "83f36d8236bcbdef4c93",
+    "url": "/instag/static/js/runtime-main.0f481324.js"
+  }
+]);
